31fef255eab4029b90e423372c58c82c4324e419
Gello Guiam <acguiam@gmail.com>
2021-06-01 21:22:59 +0800

    Add initial files

---
+++ index.py
@@ -0,0 +1,177 @@
+import cv2
+import numpy as np
+import sys
+
+# from tracker import *
+
+#decalare constants here
+INPUT_VID_FILENAME = "data//test-2.mp4"
+INPUT_VID_FILENAME = "asset/physci-edited-480p.mov"
+OUTPUT_VID_FILENAME = "sample.mov"
+# tracker = EuclideanDistTracker()
+colors = {}
+
+
+def preprocess(image):                              #enhance video quality before background subtraction
+    clahe = cv2.createCLAHE(clipLimit = 1.0)          #apply CLAHE
+    enhanced_img = clahe.apply(image)
+    return enhanced_img
+
+
+def clahe_img(image):
+    hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    clahe = cv2.createCLAHE(clipLimit = 1.0)
+
+    channels = cv2.split(hsv_img)
+    channels[0] = clahe.apply(channels[0])
+    channels[1] = clahe.apply(channels[1])
+    channels[2] = clahe.apply(channels[2])
+
+    hsv_img = cv2.merge(channels)
+    hsv_img = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2BGR)
+
+    img = cv2.detailEnhance(hsv_img, sigma_s=10, sigma_r=0.15)
+    return img
+
+
+def background_subtraction(curr_frame, prev_frame):
+    frame_diff = cv2.absdiff(curr_frame, prev_frame)
+    return frame_diff
+
+
+def morphological_transform(image):
+    blur = cv2.GaussianBlur(image, (5,5), 0)
+    _, th = cv2.threshold(blur, 20, 255, cv2.THRESH_BINARY)
+
+    kernel = np.ones((3, 3), np.uint8)
+    trasnformed = cv2.dilate(th, kernel, iterations=1)
+    trasnformed = cv2.morphologyEx(trasnformed, cv2.MORPH_OPEN, kernel)
+    # trasnformed = cv2.morphologyEx(trasnformed, cv2.MORPH_CLOSE, kernel)
+
+    return trasnformed
+
+
+def blob_detection(image, org_img):
+    components = cv2.connectedComponentsWithStats(image, 8, cv2.CV_32S)
+    (numLabels, labels, stats, centroids) = components
+    
+    output = org_img.copy()
+    box_ids = None
+
+    detections = []
+
+    for i in range(0, numLabels):
+        if (i==0): continue                                              #exclude backgrouund
+
+        x = stats[i, cv2.CC_STAT_LEFT]                                   #get bounding box
+        y = stats[i, cv2.CC_STAT_TOP]
+        w = stats[i, cv2.CC_STAT_WIDTH]
+        h = stats[i, cv2.CC_STAT_HEIGHT]
+        area = stats[i, cv2.CC_STAT_AREA]
+
+        if area > 200:
+            detections.append([x, y, w, h])
+
+            (centroid_x, centroid_y) = centroids[i]                          #get centroids
+            component_mask = (labels == i).astype("uint8") * 255             #get component mask
+            color_list = (list(np.random.choice(range(256), size=3)))        #generate random color for each blob
+            random_color = (int(color_list[0]), int(color_list[1]), int(color_list[2]))
+            # output[(component_mask != 0)] = random_color                      #color the blob
+
+    # boxes_ids = tracker.update(detections)
+    # for box_id in boxes_ids:
+
+    #     color_list = (list(np.random.choice(range(256), size=3)))        #generate random color for each blob
+    #     random_color = (int(color_list[0]), int(color_list[1]), int(color_list[2]))
+
+    #     x, y, w, h, id = box_id
+
+    #     color = ()
+    #     if id in colors.keys():
+    #         color = colors[id]
+    #     else:
+    #         colors[id] = random_color
+    #         color = random_color
+
+    #     cv2.putText(output, str(id), (x, y - 15), cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), 1)
+    #     cv2.rectangle(output, (x, y), (x + w, y + h), color, 1)
+
+        # cv2.rectangle(output, (x, y), (x+w, y+h), (0, 255, 0), 1)
+
+    return output
+
+
+def main():
+    cap = cv2.VideoCapture(INPUT_VID_FILENAME)              #load video stream
+    object_detector = cv2.createBackgroundSubtractorMOG2(history=100, varThreshold=40)
+
+    if not cap.isOpened():
+        print("Could not open video file " + str(INPUT_VID_FILENAME))
+        sys.exit(1)
+
+    height = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)*2)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT)*2)
+    fps = int(cap.get(cv2.CAP_PROP_FPS))
+    codec = cv2.VideoWriter_fourcc('M', 'J', 'P', 'G')
+    video_writer = cv2.VideoWriter(OUTPUT_VID_FILENAME, codec, fps, (width, height))
+
+    backSub = cv2.createBackgroundSubtractorKNN()
+
+
+    prev_frame = np.array([])                               #save previous frame
+    while(cap.isOpened()):                                  #loop all frames
+        ret, frame = cap.read()                             #capture each frame
+
+        width = int(frame.shape[1] * 2)
+        height = int(frame.shape[0] * 2)
+        dim = (width, height)
+
+        frame = cv2.resize(frame, dim, interpolation = cv2.INTER_AREA)
+        frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+
+        mask = backSub.apply(frame)
+        extracted = cv2.bitwise_and(frame, frame, mask=mask)
+
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)      #convert to grayscale
+        enhanced_img = preprocess(gray)                     #apply CLAHE
+
+        if (prev_frame.size != 0):
+            foreground = background_subtraction(enhanced_img, prev_frame)
+            transformed = morphological_transform(mask)
+            objects = blob_detection(transformed, frame)
+
+            # bg = 1 - transformed
+            fg_mask = transformed
+            bg_mask = 255 - transformed
+
+            extracted_fg = cv2.bitwise_and(frame, frame, mask=fg_mask)
+            extracted_bg = cv2.bitwise_and(frame, frame, mask=bg_mask)
+
+            extracted_fg = clahe_img(extracted_fg)
+
+            # extracted_bg = cv2.GaussianBlur(extracted_bg, (9,9), 0)
+            extracted_bg = cv2.edgePreservingFilter(extracted_bg, flags=1, sigma_s=60, sigma_r=0.1)
+
+            final = cv2.add(extracted_fg, extracted_bg)
+
+            # cv2.imshow("Original", hsv)
+            # cv2.imshow("Gray", gray)
+            # cv2.imshow("CLAHE", enhanced_img)
+            # cv2.imshow("Foreground", foreground)
+            cv2.imshow("FG", extracted_fg)
+            cv2.imshow("BG", extracted_fg)
+            cv2.imshow("Objects", final)
+
+            video_writer.write(final)
+
+            c = cv2.waitKey(1) % 0x100                      #listen for ESC key
+            if c == 27:
+                break
+
+        prev_frame = enhanced_img
+
+    cap.release()                                           #close video stream
+    cv2.destroyAllWindows()                                 #close all windows
+
+if __name__ == "__main__":
+    main()
---
+++ object_tracker.py
@@ -0,0 +1,220 @@
+import cv2
+import core.utils as utils
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+import time
+
+from absl import app, flags, logging
+from absl.flags import FLAGS
+from core.config import cfg
+from core.yolov4 import filter_boxes
+from PIL import Image
+from tensorflow.compat.v1 import ConfigProto
+from tensorflow.compat.v1 import InteractiveSession
+from tensorflow.python.saved_model import tag_constants
+
+from deep_sort import preprocessing, nn_matching
+from deep_sort.detection import Detection
+from deep_sort.tracker import Tracker
+from tools import generate_detections as gdet
+
+
+flags.DEFINE_string('framework', 'tf', '(tf, tflite, trt')
+flags.DEFINE_string('weights', './checkpoints/yolov4-416',
+                    'path to weights file')
+flags.DEFINE_integer('size', 416, 'resize images to')
+flags.DEFINE_boolean('tiny', False, 'yolo or yolo-tiny')
+flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
+flags.DEFINE_string('video', './data/video/sample.mov', 'path to input video or set to 0 for webcam')
+flags.DEFINE_string('output', None, 'path to output video')
+flags.DEFINE_string('output_format', 'XVID', 'codec used in VideoWriter when saving video to file')
+flags.DEFINE_float('iou', 0.20, 'iou threshold')
+flags.DEFINE_float('score', 0.10, 'score threshold')
+flags.DEFINE_boolean('dont_show', False, 'dont show video output')
+flags.DEFINE_boolean('info', True, 'show detailed info of tracked objects')
+flags.DEFINE_boolean('count', True, 'count objects being tracked on screen')
+
+
+def preprocess(image):
+    return image
+    hsv_img = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    clahe = cv2.createCLAHE(clipLimit = 2.0)
+
+    channels = cv2.split(hsv_img)
+    channels[0] = clahe.apply(channels[0])
+    channels[1] = clahe.apply(channels[1])
+    channels[2] = clahe.apply(channels[2])
+
+    hsv_img = cv2.merge(channels)
+    hsv_img = cv2.cvtColor(hsv_img, cv2.COLOR_HSV2BGR)
+    return hsv_img
+
+
+def main(_argv):
+    # Definition of the parameters
+    max_cosine_distance = 0.4
+    nn_budget = None
+    nms_max_overlap = 5.0
+    
+    # initialize deep sort
+    model_filename = 'model_data/mars-small128.pb'
+    encoder = gdet.create_box_encoder(model_filename, batch_size=1)
+
+    # calculate cosine distance metric
+    metric = nn_matching.NearestNeighborDistanceMetric("cosine", max_cosine_distance, nn_budget)
+
+    # initialize tracker
+    tracker = Tracker(metric)
+
+    # load configuration for object detector
+    config = ConfigProto()
+    config.gpu_options.allow_growth = True
+    session = InteractiveSession(config=config)
+    STRIDES, ANCHORS, NUM_CLASS, XYSCALE = utils.load_config(FLAGS)
+    input_size = FLAGS.size
+    video_path = FLAGS.video
+
+    saved_model_loaded = tf.saved_model.load(FLAGS.weights, tags=[tag_constants.SERVING])
+    infer = saved_model_loaded.signatures['serving_default']
+
+    # begin video capture
+    try:
+        vid = cv2.VideoCapture(int(video_path))
+    except:
+        vid = cv2.VideoCapture(video_path)
+
+    out = None
+
+    frame_num = 0
+    # while video is running
+    while True:
+        return_value, frame = vid.read()
+        if return_value:
+            frame = preprocess(frame)
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            image = Image.fromarray(frame)
+        else:
+            print('Video has ended or failed, try a different video format!')
+            break
+
+        frame_num +=1
+        print('Frame #: ', frame_num)
+        image_data = cv2.resize(frame, (input_size, input_size))
+        image_data = image_data / 255.
+        image_data = image_data[np.newaxis, ...].astype(np.float32)
+        start_time = time.time()
+
+        batch_data = tf.constant(image_data)
+        pred_bbox = infer(batch_data)
+        for key, value in pred_bbox.items():
+            boxes = value[:, :, 0:4]
+            pred_conf = value[:, :, 4:]
+
+        boxes, scores, classes, valid_detections = tf.image.combined_non_max_suppression(
+            boxes=tf.reshape(boxes, (tf.shape(boxes)[0], -1, 1, 4)),
+            scores=tf.reshape(
+                pred_conf, (tf.shape(pred_conf)[0], -1, tf.shape(pred_conf)[-1])),
+            max_output_size_per_class=50,
+            max_total_size=50,
+            iou_threshold=FLAGS.iou,
+            score_threshold=FLAGS.score
+        )
+
+        # convert data to numpy arrays and slice out unused elements
+        num_objects = valid_detections.numpy()[0]
+        bboxes = boxes.numpy()[0]
+        bboxes = bboxes[0:int(num_objects)]
+        scores = scores.numpy()[0]
+        scores = scores[0:int(num_objects)]
+        classes = classes.numpy()[0]
+        classes = classes[0:int(num_objects)]
+
+        # format bounding boxes from normalized ymin, xmin, ymax, xmax ---> xmin, ymin, width, height
+        original_h, original_w, _ = frame.shape
+        bboxes = utils.format_boxes(bboxes, original_h, original_w)
+
+        # store all predictions in one parameter for simplicity when calling functions
+        pred_bbox = [bboxes, scores, classes, num_objects]
+
+        # read in all class names from config
+        class_names = utils.read_class_names(cfg.YOLO.CLASSES)
+
+        # by default allow all classes in .names file
+        # allowed_classes = list(class_names.values())        
+        allowed_classes = ['person']
+
+        # loop through objects and use class index to get class name, allow only classes in allowed_classes list
+        names = []
+        deleted_indx = []
+        for i in range(num_objects):
+            class_indx = int(classes[i])
+            class_name = class_names[class_indx]
+            if class_name not in allowed_classes:
+                deleted_indx.append(i)
+            else:
+                names.append(class_name)
+        names = np.array(names)
+        count = len(names)
+
+        # delete detections that are not in allowed_classes
+        bboxes = np.delete(bboxes, deleted_indx, axis=0)
+        scores = np.delete(scores, deleted_indx, axis=0)
+
+        count = len(scores)
+        cv2.putText(frame, "Tracking {} people.".format(count), (10, 50), cv2.FONT_HERSHEY_PLAIN, 1, (0, 255, 0), 1)
+
+        # encode yolo detections and feed to tracker
+        features = encoder(frame, bboxes)
+        detections = [Detection(bbox, score, class_name, feature) for bbox, score, class_name, feature in zip(bboxes, scores, names, features)]
+
+        # initialize color map
+        cmap = plt.get_cmap('tab20b')
+        colors = [cmap(i)[:3] for i in np.linspace(0, 1, 20)]
+
+        # run non-maxima supression
+        boxs = np.array([d.tlwh for d in detections])
+        scores = np.array([d.confidence for d in detections])
+        classes = np.array([d.class_name for d in detections])
+        indices = preprocessing.non_max_suppression(boxs, classes, nms_max_overlap, scores)
+        detections = [detections[i] for i in indices]       
+
+        # Call the tracker
+        tracker.predict()
+        tracker.update(detections)
+
+        # update tracks
+        for track in tracker.tracks:
+            if not track.is_confirmed() or track.time_since_update > 1:
+                continue 
+            bbox = track.to_tlbr()
+            class_name = track.get_class()
+            
+        # draw bbox on screen
+            color = colors[int(track.track_id) % len(colors)]
+            color = [i * 255 for i in color]
+            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1])), (int(bbox[2]), int(bbox[3])), color, 2)
+            cv2.rectangle(frame, (int(bbox[0]), int(bbox[1]-30)), (int(bbox[0])+(len(str(track.track_id)))*17, int(bbox[1])), color, -1)
+            cv2.putText(frame, str(track.track_id),(int(bbox[0]), int(bbox[1]-10)),0, 0.75, (255,255,255), 2)
+
+            print("Tracker ID: {}, Class: {},  BBox Coords (xmin, ymin, xmax, ymax): {}".format(str(track.track_id), class_name, (int(bbox[0]), int(bbox[1]), int(bbox[2]), int(bbox[3]))))
+
+        # calculate frames per second of running detections
+        fps = 1.0 / (time.time() - start_time)
+        print("FPS: %.2f" % fps)
+        result = np.asarray(frame)
+        result = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+        
+        cv2.imshow("Output Video", result)
+        
+        # if output flag is set, save video file
+        if FLAGS.output:
+            out.write(result)
+        if cv2.waitKey(1) & 0xFF == ord('q'): break
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    try:
+        app.run(main)
+    except SystemExit:
+        pass
